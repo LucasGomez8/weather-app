99e9d20584eff2989b8ca8ebe2058d4d01f0dadb
Lucas <lucgom99@gmail.com>
2021-11-28 23:57:01 -0300

    update
    Changes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,26 +1,90 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-import Winfo from "./components/WInfo";
-import Wform from "./components/Wform";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  getWeather = (e) => {
-    console.log("submiting");
+const api = {
+  key: "7135c9c4c61e42d1d94e2fb276d02bb7",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const search = (evt) => {
+    if (evt.ket === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+        });
+    }
   };
 
-  render() {
-    return (
-      <div className="container p-4">
-        <div className="row">
-          <div className="col-md-6 mx-auto">
-            <Wform getWeather={this.getWeather} />
-            <Winfo />
-          </div>
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-      </div>
-    );
-  }
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">15°c</div>
+              <div className="weather">Sunny</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- weather-app/src/components/WInfo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Winfo = () => {
-  return (<h1>Hello</h1>);
-};
-
-export default Winfo;
