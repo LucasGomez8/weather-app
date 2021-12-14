5e2f157ccc92070cba1f06cd5b5a0ee36fe8fd0b
Lucas <lucgom99@gmail.com>
2021-12-13 21:32:03 -0300

    Update
    Correccion de errores y busqueda de lugares

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [weather, setWeather] = useState("");
 
   const search = (evt) => {
-    if (evt.ket === "Enter") {
+    if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
@@ -54,7 +54,7 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   };
   return (
-    <div className="app">
+    <div className={(typeof weather.main !="undefined") ? ((weather.main.temp > 16) ? "app-warm" : "app") : "app"}>
       <main>
         <div className="search-box">
           <input
@@ -75,8 +75,8 @@ function App() {
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">15°c</div>
-              <div className="weather">Sunny</div>
+              <div className="temp">{Math.round(weather.main.temp)}°</div>
+              <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
         ) : (
